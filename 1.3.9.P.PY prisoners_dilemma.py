6311bb655cce0b0d5ed2e1947219655e0de56468
adsotomayor <asotomayor@pinecrestacademy.org>
2014-06-20 08:45:55 -0400

    Added Bad to the Bone
    Player 4 is now ours. And yes, he will win!!!!

--- 1.3.9.P.PY prisoners_dilemma.py
+++ 1.3.9.P.PY prisoners_dilemma.py
@@ -204,19 +204,40 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     #
     elif player == 4:
         if getting_team_name:
-            return 'betray every 3rd round'
+            return 'Bad To The Bone'
+        
         else:
             # use history, opponent_history, score, opponent_score
-            # to compute your strategy
-            size = len(history)
-            if(size%3==0): #the number of rounds played is a multiple of 3
+            # to compute your strategy      
+            if len(opponent_history)==0: #It's the first round: collude
                 return 'c'
             else:
-                return 'b'
-    
-    
-    
-
+                # if there was a previous round just like the last one,
+                # do whatever they did in the round that followed it
+                recent_round_opponent = opponent_history[-1]
+                recent_round_me = history[-1]
+                            
+                #go through rounds before that one
+                for round in range(len(history)-1):
+                    prior_round_opponent = opponent_history[round]
+                    prior_round_me = history[round]
+                    #if one matches
+                    if (prior_round_me == recent_round_me) or (prior_round_opponent == recent_round_opponent):
+                        return opponent_history[round]
+                # no match found
+                if history[-1]=='c' and opponent_history[-1]=='b':
+                    return 'b' # betray is they were severely punished last time
+                else:
+                    return 'c' #otherwise collude        
+        '''else:
+            # use history, opponent_history, score, opponent_score
+            # to compute your strategy
+            if len(opponent_history)==0: #It's the first round: collude
+                return 'c'
+            elif history[-1]=='c' and opponent_history[-1]=='b':
+                return 'b' # betray is they were severely punished last time
+            else:
+                return 'c' #otherwise collude'''
 
 
 
